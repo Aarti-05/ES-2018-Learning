63b6e82b8f94288ed330e83c6f4a38d15d3cbe53
unknown <abc@gmail.com>
2020-06-23 15:22:20 +0530

    assignment operators

---
+++ assign.js
@@ -0,0 +1,21 @@
+/**** Equal to operator *****/ 
+let x= 10;
+console.log(x);
+/**** Add and assign*****/
+ x += 5;
+console.log(x);
+/**** Sub  and assign*****/
+x -= 5;
+console.log(x);
+/**** Mul and assign*****/
+x *= 5;
+console.log(x);
+/**** Div and assign*****/
+x /= 5;
+console.log(x);
+/**** Mod and assign*****/
+x %= 5;
+console.log(x);
+/**** Exp and assign*****/
+x **= 5;
+console.log(x);
