a7b38bb452adfa90121d1ba006f82b9bfb4b6a3d
unknown <abc@gmail.com>
2020-06-23 16:09:02 +0530

    conditional statements

---
+++ condition.js
@@ -0,0 +1,16 @@
+let x= 50;
+let y= 30;
+if (x>y)
+{
+    console.log("x is greater than y");
+}
+else if(x==y)
+{
+    console.log("x is equal to y");   
+}
+else
+{
+    console.log(" not  a true statement");
+}
+
+    
