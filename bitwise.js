32814856bd057b0bb69d445c9511d5644d49f1e2
unknown <abc@gmail.com>
2020-06-23 15:55:49 +0530

    bitwise oprators

---
+++ bitwise.js
@@ -0,0 +1,14 @@
+/** & operator **/
+let x=5;
+let y=10;
+console.log(x & y);
+/** | operator **/
+console.log(x | y);
+/** ~ NOT operator **/
+console.log(~x);
+/** ^ XOR operator **/
+console.log(x ^ y);
+/** << left shift operator **/
+console.log(x << 1);
+/** << Right shift operator **/
+console.log(x >> 1);
