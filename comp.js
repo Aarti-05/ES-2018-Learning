f2a8ad8972e6268c6ee59af0e3b29e68a0b738cf
unknown <abc@gmail.com>
2020-06-23 15:34:31 +0530

    assignment operator

---
+++ comp.js
@@ -0,0 +1,15 @@
+/**** Double Equals to****/
+let x=10;
+let y=10;
+console.log(x==y)
+/**** Tripple Equals to****/
+let z='10'
+console.log(x===z);
+/**** Not Equal to****/
+console.log(x!=y);
+/**** Not Double Equals to****/
+console.log(x!==z);
+/**** Greater than Equals to****/
+console.log(x>=y)
+/**** Less Than Equals to****/
+console.log(x<=y)
