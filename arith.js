30afe6818f2028b3bdf8a3462d36c0464f1501e0
unknown <abc@gmail.com>
2020-06-23 14:56:20 +0530

    arithmatic operators

---
+++ arith.js
@@ -0,0 +1,24 @@
+/****** Addition   ********/
+let num1 = 10;
+let num2 = 5;
+console.log("result is :"+(num1+num2));
+
+/****** Subtraction*******/
+console.log("result is :"+(num1 - num2));
+
+/****** Multiplication *******/
+console.log("result is :"+(num1 * num2));
+/****** Division *******/
+console.log("result is :"+(num1 / num2));
+/****** Modulus *******/
+console.log("result is :"+(num1 % num2));
+/****** Exponential *******/
+console.log("result is :"+(num1 ** num2));
+/****** increment *******/
+num2 ++;
+
+console.log("result is :"+(num2));
+/****** Decrement *******/
+--num1;
+console.log("result is :"+(num1));
+/****** Predecrement *******/
