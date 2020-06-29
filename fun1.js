7e40741504053315f703b7f734a6fa4cc694c79d
unknown <abc@gmail.com>
2020-06-29 18:36:07 +0530

    print the day

---
+++ fun1.js
@@ -0,0 +1,14 @@
+function printDay(a){
+   let obj={
+        1:"Sunday",
+        2:"Monday",
+        3:"Tuesday",
+        4:"Wednesday",
+        5:"Thursday",
+        6:"Friday",
+        7:"Saturday"
+
+   }
+   return obj[a];
+}
+console.log(printDay(1));
