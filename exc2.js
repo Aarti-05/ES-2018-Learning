7952c562055dd7959c891cd764264918449e5b55
unknown <abc@gmail.com>
2020-06-30 10:55:29 +0530

    exercise2

---
+++ exc2.js
@@ -0,0 +1,13 @@
+let today=new Date();
+let day=today.getDate();
+let month=today.getMonth()+1;
+let year=today.getFullYear();
+if (day<10){
+    day = "0"+day;
+}
+if (month<10){
+    month = "0"+month;
+}
+
+
+console.log(`${day}-${month}-${year}`);
