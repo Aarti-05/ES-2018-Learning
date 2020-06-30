9ffa6d2850bb59ee25c4b1539c00136fd5a4cebe
unknown <abc@gmail.com>
2020-06-30 14:50:54 +0530

    exercise 7

---
+++ exc7.js
@@ -0,0 +1,10 @@
+let x= Number(prompt("enter any number from 1-10 : "))
+let y= Math.ceil(Math.random());
+// if(x===y){
+// console.log("Good Job");
+// }
+// else{
+// console.log("Bad luck");
+// }
+(x===y)?"good":"bad";
+
