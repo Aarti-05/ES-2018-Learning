922417b96170f8a1c82cc6f96c3ac58ff61467d9
unknown <abc@gmail.com>
2020-06-30 11:33:32 +0530

    exercise5

---
+++ exc5.js
@@ -0,0 +1,8 @@
+let today=new Date();
+let year= today.getFullYear();
+if((year%100) && (year%400)){
+    console.log("The year is a leap year")
+}
+else{
+    console.log("The year is a leap year")
+}
