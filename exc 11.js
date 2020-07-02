28b6c986b28c8f617f6f2ed07518b83460d8d621
unknown <abc@gmail.com>
2020-07-02 15:07:08 +0530

    multiple object

---
+++ exc 11.js
@@ -0,0 +1,26 @@
+// function add(a,b){
+//     return {
+//          sum:a+b
+        
+        
+//     };
+// }
+// const sum1=add(6,5);
+// console.log(sum1);
+function employee(name,salary,experience,department){
+    return {
+        name,
+        salary,
+        experience,
+        department
+    };
+}
+const employee1=employee("john",15000,5,"engineering");
+const employee2=employee("jane",15000,5,"engineering");
+const employee3=employee("slacey",15000,5,"engineering");
+console.log(employee1);
+console.log(employee2);
+console.log(employee3);
+
+
+    
