159e06669a96d5da11cc948133abadf892635b3f
unknown <abc@gmail.com>
2020-07-07 15:40:15 +0530

    nestedobjexe4

--- yam.js
+++ yam.js
@@ -70,30 +70,59 @@
 // }
 // addDetails("isHillarious",true);
 // console.log(instructorData);
-var nestedData = {
-    innerData: {
-      order: ["first", "second", "third"],
-      snacks: ["chips", "fruit", "crackers"],
-      numberData: {
-          primeNumbers: [2,3,5,7,11],
-          fibonnaci: [1,1,2,3,5,8,13]
+// var nestedData = {
+//     innerData: {
+//       order: ["first", "second", "third"],
+//       snacks: ["chips", "fruit", "crackers"],
+//       numberData: {
+//           primeNumbers: [2,3,5,7,11],
+//           fibonnaci: [1,1,2,3,5,8,13]
+//       },
+//       addSnack: function(snack){
+//           this.snacks.push(snack);
+//           return this;
+//       }
+//     }
+//   }
+//   for (let key in nestedData.innerData.numberData.primeNumbers){
+//       console.log(nestedData.innerData.numberData.primeNumbers[key])
+//   }
+//   for (let key in nestedData.innerData.numberData.fibonnaci){
+//     console.log(nestedData.innerData.numberData.fibonnaci[key])
+// }
+// console.log(nestedData.innerData.order[1]);
+// console.log(nestedData.innerData.addSnack("chocalate"));
+// console.log(nestedData.innerData.snacks);
+var nestedObject = {
+    speakers: [{name:"Elie"},{name:"Tim"},{name:"Matt"}],
+    data: {
+      continents: {
+        europe: {
+          countries: {
+            switzerland: {
+              capital: "Bern",
+              population: 8000000
+            }
+          }
+        }
       },
-      addSnack: function(snack){
-          this.snacks.push(snack);
-          return this;
+      languages: {
+        spanish: {
+            hello: "Hola"
+        },
+        french: {
+            hello: "Bonjour"
+        }
       }
     }
   }
-  for (let key in nestedData.innerData.numberData.primeNumbers){
-      console.log(nestedData.innerData.numberData.primeNumbers[key])
-  }
-  for (let key in nestedData.innerData.numberData.fibonnaci){
-    console.log(nestedData.innerData.numberData.fibonnaci[key])
-}
-console.log(nestedData.innerData.order[1]);
-console.log(nestedData.innerData.addSnack("chocalate"));
-console.log(nestedData.innerData.snacks);
+  const addSpeaker=(key,value)=>{
+    //key:value;
+      nestedObject.speakers.push({key:value});
 
+  }
+  addSpeaker(name,"RIAZ");
+ console.log(nestedObject.speakers) 
 
 
 
