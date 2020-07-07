686231116e793ebfe70258cef430f19ed2d1fe03
unknown <abc@gmail.com>
2020-07-07 14:29:52 +0530

    nestedobj exe1

--- yam.js
+++ yam.js
@@ -33,44 +33,60 @@
 
 
 //console.log(instructorData.favoriteHobbies[0])
-var instructorData = {
-    name: "Elie",
-    additionalData: {
-        instructor: true,
-        favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
-        moreDetails: {
-            favoriteBasketballTeam: "New York Knicks",
-            numberOfSiblings: 3,
-            isYoungest: true,
-            hometown: {
-                city: "West Orange",
-                state: "NJ",
-            },
-            citiesLivedIn: ["Seattle", "Providence", "New York"]
-        }
-    }
-};
-function displayCities(){
-    //for (let key in instructorData.additionalData.moreDetails){
-        console.log(instructorData.additionalData.moreDetails["citiesLivedIn"]);
-    }
+// var instructorData = {
+//     name: "Elie",
+//     additionalData: {
+//         instructor: true,
+//         favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
+//         moreDetails: {
+//             favoriteBasketballTeam: "New York Knicks",
+//             numberOfSiblings: 3,
+//             isYoungest: true,
+//             hometown: {
+//                 city: "West Orange",
+//                 state: "NJ",
+//             },
+//             citiesLivedIn: ["Seattle", "Providence", "New York"]
+//         }
+//     }
+// };
+// function displayCities(){
+//     //for (let key in instructorData.additionalData.moreDetails){
+//         console.log(instructorData.additionalData.moreDetails["citiesLivedIn"]);
+//     }
 
-displayCities();
-const homeTown=()=>{
-    //for(let key in instructorData.additionalData.moreDetails.hometown){
-      let cities=(instructorData.additionalData.moreDetails.hometown);
-      for (let key in cities){
-          console.log(cities[key])
+// displayCities();
+// const homeTown=()=>{
+//     //for(let key in instructorData.additionalData.moreDetails.hometown){
+//       let cities=(instructorData.additionalData.moreDetails.hometown);
+//       for (let key in cities){
+//           console.log(cities[key])
+//       }
+//     }
+
+// homeTown();
+// const addDetails = (key,value)=>{
+//    instructorData[key]=value;
+// }
+// addDetails("isHillarious",true);
+// console.log(instructorData);
+var nestedData = {
+    innerData: {
+      order: ["first", "second", "third"],
+      snacks: ["chips", "fruit", "crackers"],
+      numberData: {
+          primeNumbers: [2,3,5,7,11],
+          fibonnaci: [1,1,2,3,5,8,13]
+      },
+      addSnack: function(snack){
+          this.snacks.push(snack);
+          return this;
       }
     }
-
-homeTown();
-const addDetails = (key,value)=>{
-   instructorData[key]=value;
-}
-addDetails("isHillarious",true);
-console.log(instructorData);
-
+  }
+  for (let key in nestedData.innerData.numberData.primeNumbers){
+      console.log(nestedData.innerData.numberData.primeNumbers[key])
+  }
 
 
 
