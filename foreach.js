e268e75ce77225f745ba28f0ef16e7efdfb2ad81
unknown <abc@gmail.com>
2020-07-11 12:31:40 +0530

    concatall

--- foreach.js
+++ foreach.js
@@ -19,28 +19,71 @@ const getStockSymbol=(arr)=>{
 
 //return stock;
 }
-let arr=[
-    {
-    symbols:"zxc",price:240.89,volume:253698
-    },
-    {
-    symbols:"abc",price:200.09,volume:2698
-    },
-    {
-    symbols:"mnp",price:140.89,volume:253
-    }
-];
-console.log(getStockSymbol(arr));
-//let stock;
-let stock=arr.filter((value)=>{
-if(value.price>200){
-    return value.price;
-};
-}).map((value)=>{
-    return value.symbols;
-});
-console.log(stock)
-stock.forEach((value)=>{
+// 
+   
+// //     console.log(getStockSymbol(arr));
+// // //let stock;
+// // let stock=arr.filter((value)=>{
+// //     return value.price>200;
+// // }).map((value)=>{
+// //     return value.symbols;
+// // });
+// // stock.forEach((value)=>{
 
-    console.log(stock)
+// //     console.log(stock)
+// // })
+let arr=[
+         [
+           {symbols:"zxc",price:240.89,volume:253698},
+            {symbols:"zxc",price:240.89,volume:253698}
+        ],
+           [
+            {symbols:"abc",price:200.09,volume:2698},
+            {symbols:"mnp",price:140.89,volume:253 }
+            ]
+    ];
+Array.prototype.concatAll=function(){
+ let results=[];
+    this.forEach((subArray)=>{
+        subArray.forEach((item)=>{
+        results.push(item);
+    });
+    });
+    return results;
+};
+let stocks=arr.concatAll();
+stocks.forEach((value)=>{
+    console.log(value);
 })
+// var exchanges = [
+//     [
+//         { symbol: "XFX", price: 240.22, volume: 23432 },
+//         { symbol: "TNZ", price: 332.19, volume: 234 }
+//       ],
+//     [
+//         { symbol: "JXJ", price: 120.22, volume: 5323 },
+//         { symbol: "NYN", price: 88.47, volume: 98275 }
+//       ]
+//   ];
+  
+  
+//   Array.prototype.concatAll = function() {
+//     var results = [];
+    
+//     this.forEach(function(subArray) {
+//       subArray.forEach(function(item) {
+//         results.push(item);    
+//       });
+//     });  
+  
+//     return results;
+//   };
+  
+  
+//   var stocks =  exchanges.concatAll();
+      
+//   stocks.forEach(function(stock) {       
+//     console.log(JSON.stringify(stock));  
+//     // display in plunker preview
+//     //displayInPreview(JSON.stringify(stock))
+//   });
