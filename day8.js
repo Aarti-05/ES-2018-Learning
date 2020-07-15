64eeb34b0fd8b4694b6ff588c899c2a53307d022
unknown <abc@gmail.com>
2020-07-15 11:40:30 +0530

    Day 8 function invocation using call

--- day8.js
+++ day8.js
@@ -4,3 +4,10 @@ function add(x,y){
 }
 const a= add(2,5)   /////invoking function add by passing parameters /////
 console.log(a)   //////printing the result of function add////
+
+///******** function invocation usin call,apply and bind******* */
+function multiply(x,y){
+    return x*y;
+}
+const z=multiply.call(null,45,2,3);///function invocation using call method//////
+console.log(z)
